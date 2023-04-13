eea2c538aea4e5b73781643eb135861620a43435
uniqcle <uniqcle@yandex.ru>
2023-04-13 13:55:23 +0700

    mobile nav

---
+++ js/mobile-nav.js
@@ -0,0 +1,20 @@
+// Mobile nav button
+const navBtn = document.querySelector('.mobile-nav-btn');
+const nav = document.querySelector('.mobile-nav');
+const fade = document.querySelector('.mobile-nav-fade');
+const menuIcon = document.querySelector('.nav-icon');
+
+navBtn.onclick = function () {
+    nav.classList.toggle('mobile-nav--open');
+    fade.classList.toggle('mobile-nav-fade--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+};
+
+
+fade.onclick = function () {
+    nav.classList.toggle('mobile-nav--open');
+    fade.classList.toggle('mobile-nav-fade--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+};
